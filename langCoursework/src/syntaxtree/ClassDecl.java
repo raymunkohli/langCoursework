d4626b84c44021cdfabcf1f15edf74c4fb5c81fb
Raymun <Raymun1@hotmail.co.uk>
2018-03-07 08:56:14 +0000

    deleted duped folder

--- langCoursework/src/syntaxtree/ClassDecl.java
+++
@@ -1,9 +0,0 @@
-package syntaxtree;
-
-import visitor.Visitor;
-
-public abstract class ClassDecl extends AST {
-
-    public abstract <T> T accept(Visitor<T> v);
-    
-}
--- langCoursework/src/syntaxtree/Exp.java
+++
@@ -1,8 +0,0 @@
-package syntaxtree;
-
-import visitor.Visitor;
-
-public abstract class Exp extends AST {
-
-    public abstract <T> T accept(Visitor<T> v);
-}
